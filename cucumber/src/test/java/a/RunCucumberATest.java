9cded32dcde2757d004a141e8dcb7520d2882e05
M.P. Korstanje <rien.korstanje@gmail.com>
2025-06-04 20:28:59 +0200

    Touch ups

--- cucumber/src/test/java/a/RunCucumberATest.java
+++ cucumber/src/test/java/a/RunCucumberATest.java
@@ -4,14 +4,14 @@ import io.cucumber.java.en.Given;
 import org.assertj.core.api.Assertions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("a")
+@SelectClasspathResource("belly.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "a")
 public class RunCucumberATest {
 
--- cucumber/src/test/java/b/RunCucumberBTest.java
+++ cucumber/src/test/java/b/RunCucumberBTest.java
@@ -3,14 +3,14 @@ package b;
 import io.cucumber.java.en.Given;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("b")
+@SelectClasspathResource("belly.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "b")
 public class RunCucumberBTest {
 
